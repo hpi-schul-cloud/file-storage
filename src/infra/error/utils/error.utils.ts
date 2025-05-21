53d5679ccde9ca565f63513bf099b1859cdaa17a
SevenWaysDP <sergej.hoffmann@dataport.de>
2025-05-21 09:58:53 +0200

    feat: implement comprehensive error handling and logging system with global error filter and loggable exceptions

---
+++ src/infra/error/utils/error.utils.ts
@@ -0,0 +1,19 @@
+import { HttpException, HttpExceptionOptions } from '@nestjs/common';
+
+export class ErrorUtils {
+	public static isNestHttpException(error: unknown): error is HttpException {
+		return error instanceof HttpException;
+	}
+
+	public static createHttpExceptionOptions(error: unknown, description?: string): HttpExceptionOptions {
+		let causeError: Error | undefined;
+
+		if (error instanceof Error) {
+			causeError = error;
+		} else {
+			causeError = error ? new Error(JSON.stringify(error)) : undefined;
+		}
+
+		return { cause: causeError, description };
+	}
+}
