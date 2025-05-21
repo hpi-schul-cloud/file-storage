0908b81f5d8f677c1225943f307a13bc20b11023
SevenWaysDP <sergej.hoffmann@dataport.de>
2025-05-21 14:32:25 +0200

    feat: implement logging and swagger setup for Files Storage API

--- src/apps/files-storage.app.ts
+++ src/apps/files-storage.app.ts
@@ -1,51 +1,35 @@
 /* istanbul ignore file */
 /* eslint-disable no-console */
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
 
 // register source-map-support for debugging
 import { install as sourceMapInstall } from 'source-map-support';
 
 // application imports
-import { LegacyLogger } from '@core/logger';
+import { LoggerConfig } from '@infra/logger';
 import { FilesStorageApiModule } from '@modules/files-storage/files-storage-api.app.module';
-import { SwaggerDocumentOptions } from '@nestjs/swagger';
-import { enableOpenApiDocs, createRequestLoggerMiddleware } from './helpers';
+import { createRequestLoggerMiddleware, enableOpenApiDocs } from './helpers';
 
 async function bootstrap(): Promise<void> {
 	sourceMapInstall();
 
-	// create the NestJS application on a seperate express instance
-	const nestExpress = express();
-
-	const nestExpressAdapter = new ExpressAdapter(nestExpress);
-	const nestApp = await NestFactory.create(FilesStorageApiModule, nestExpressAdapter);
-
-	// WinstonLogger
-	const legacyLogger = await nestApp.resolve(LegacyLogger);
-	nestApp.useLogger(legacyLogger);
+	const nestApp = await NestFactory.create(FilesStorageApiModule);
 
 	// customize nest app settings
 	nestApp.enableCors({ exposedHeaders: ['Content-Disposition'] });
-	nestApp.use(createRequestLoggerMiddleware());
 
-	const options: SwaggerDocumentOptions = {
-		operationIdFactory: (_controllerKey: string, methodKey: string) => methodKey,
-	};
-	enableOpenApiDocs(nestApp, 'docs', options);
+	const config = nestApp.get(LoggerConfig);
 
-	await nestApp.init();
+	nestApp.use(createRequestLoggerMiddleware(config.LOGGER_GLOBAL_REQUEST_LOGGING_ENABLED));
 
-	// mount instances
-	const rootExpress = express();
+	enableOpenApiDocs(nestApp, 'api/v3/docs');
 
 	const port = 4444;
 	const basePath = '/api/v3';
 
-	// exposed alias mounts
-	rootExpress.use(basePath, nestExpress);
-	rootExpress.listen(port);
+	nestApp.setGlobalPrefix(basePath);
+	await nestApp.init();
+	nestApp.listen(port);
 
 	console.log('#################################');
 	console.log(`### Start Files Storage Server   ###`);
