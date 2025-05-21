89202c3e399365fadde5bd2420d89c2eaea1436d
SevenWaysDP <sergej.hoffmann@dataport.de>
2025-05-21 10:37:24 +0200

    feat: Implement entity factories for user and account management
    - Added EntityFactory class for creating entities with predefined properties.
    - Introduced JwtAuthenticationFactory for generating JWT tokens.
    - Created UserAndAccountTestFactory to build user and account entities for testing.
    - Developed role factory for managing user roles.
    - Implemented user factory to handle different user roles and permissions.
    - Added test API client for simplified testing of API endpoints with JWT and API key authentication.
    - Created comprehensive tests for the test API client covering various HTTP methods and authentication scenarios.

---
+++ src/testing/factory/account.factory.ts
@@ -0,0 +1,32 @@
+/* istanbul ignore file */
+import { ObjectId } from '@mikro-orm/mongodb';
+import { DeepPartial } from 'fishery';
+import { AccountEntity } from '../entity/account.entity';
+import { UserEntity } from '../entity/user.entity';
+import { BaseFactory } from './base.factory';
+
+export const defaultTestPassword = 'DummyPasswd!1';
+export const defaultTestPasswordHash = '$2a$10$/DsztV5o6P5piW2eWJsxw.4nHovmJGBA.QNwiTmuZ/uvUc40b.Uhu';
+class AccountFactory extends BaseFactory<AccountEntity, AccountEntity> {
+	withUser(user: UserEntity): this {
+		if (!user.id) {
+			throw new Error('User does not have an id.');
+		}
+
+		const params: DeepPartial<AccountEntity> = { userId: user.id };
+
+		return this.params(params);
+	}
+}
+
+// !!! important username should not be contain a space !!!
+export const accountFactory = AccountFactory.define(AccountEntity, ({ sequence }) => {
+	const _id = new ObjectId();
+	return {
+		_id,
+		id: _id.toHexString(),
+		username: `account#${sequence}@example.tld`,
+		password: defaultTestPasswordHash,
+		userId: new ObjectId(),
+	};
+});
