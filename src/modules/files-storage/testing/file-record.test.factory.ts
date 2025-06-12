0f07439dd282e39ae535f1cabbd1c9bceef885e3
Max Bischof <maximilian.bischof@dataport.de>
2025-06-12 19:02:06 +0200

    Remove export of FileRecordTestFactory

--- src/modules/files-storage/testing/file-record.test.factory.ts
+++ src/modules/files-storage/testing/file-record.test.factory.ts
@@ -10,7 +10,7 @@ import {
 } from '../domain';
 import { FileRecordFactory } from '../domain/file-record.factory';
 
-export class FileRecordTestFactory {
+class FileRecordTestFactory {
 	private sequence = 0;
 
 	props: FileRecordProps = {
