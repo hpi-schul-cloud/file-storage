9ac9b6e8a7944123378d0c6070472ca537d52ef6
Max Bischof <106820326+bischofmax@users.noreply.github.com>
2025-07-23 16:34:09 +0200

    BC-10100 - Add defaults for WOPI_URL and COLLABORA_ONLINE_URL (#48)

--- src/infra/collabora/collabora.config.ts
+++ src/infra/collabora/collabora.config.ts
@@ -2,5 +2,5 @@ import { IsUrl } from 'class-validator';
 
 export class CollaboraConfig {
 	@IsUrl({ require_tld: false })
-	COLLABORA_ONLINE_URL!: string;
+	COLLABORA_ONLINE_URL = 'http://localhost:9980';
 }
