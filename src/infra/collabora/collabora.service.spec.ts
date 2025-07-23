be81cf9e3f757fb929375fbbd369bc404e58dec7
Max Bischof <maximilian.bischof@dataport.de>
2025-07-23 11:21:59 +0200

    Refactor CollaboraService tests for improved readability and structure

--- src/infra/collabora/collabora.service.spec.ts
+++ src/infra/collabora/collabora.service.spec.ts
@@ -10,207 +10,205 @@ import { CollaboraConfig } from './collabora.config';
 import { CollaboraService } from './collabora.service';
 
 describe('CollaboraService', () => {
-	describe('CollaboraService', () => {
-		let service: CollaboraService;
-		let collaboraConfig: DeepMocked<CollaboraConfig>;
-		let httpService: DeepMocked<HttpService>;
-		let module: TestingModule;
+	let service: CollaboraService;
+	let collaboraConfig: DeepMocked<CollaboraConfig>;
+	let httpService: DeepMocked<HttpService>;
+	let module: TestingModule;
 
-		afterEach(() => {
-			jest.resetAllMocks();
-		});
+	afterEach(() => {
+		jest.resetAllMocks();
+	});
 
-		beforeAll(async () => {
-			collaboraConfig = createMock<CollaboraConfig>();
-			httpService = createMock<HttpService>();
+	beforeAll(async () => {
+		collaboraConfig = createMock<CollaboraConfig>();
+		httpService = createMock<HttpService>();
 
-			module = await Test.createTestingModule({
-				providers: [
-					CollaboraService,
-					{ provide: HttpService, useValue: httpService },
-					{ provide: CollaboraConfig, useValue: collaboraConfig },
-				],
-			}).compile();
+		module = await Test.createTestingModule({
+			providers: [
+				CollaboraService,
+				{ provide: HttpService, useValue: httpService },
+				{ provide: CollaboraConfig, useValue: collaboraConfig },
+			],
+		}).compile();
 
-			service = module.get(CollaboraService);
-		});
+		service = module.get(CollaboraService);
+	});
 
-		afterAll(async () => {
-			await module.close();
-		});
+	afterAll(async () => {
+		await module.close();
+	});
 
-		describe('discoverUrl', () => {
-			describe('when httpService.get resolves', () => {
-				describe('when status is 200', () => {
-					const setup = () => {
-						const mimeType = 'application/vnd.oasis.opendocument.text';
-						const urlsrc = 'https://collabora.test/hosting/discovery';
-						const xmlString = `<wopi-discovery><net-zone><app name='${mimeType}'><action urlsrc='${urlsrc}'/></app></net-zone></wopi-discovery>`;
-						const response = axiosResponseFactory.build({ data: xmlString, status: 200 });
+	describe('discoverUrl', () => {
+		describe('when httpService.get resolves', () => {
+			describe('when status is 200', () => {
+				const setup = () => {
+					const mimeType = 'application/vnd.oasis.opendocument.text';
+					const urlsrc = 'https://collabora.test/hosting/discovery';
+					const xmlString = `<wopi-discovery><net-zone><app name='${mimeType}'><action urlsrc='${urlsrc}'/></app></net-zone></wopi-discovery>`;
+					const response = axiosResponseFactory.build({ data: xmlString, status: 200 });
 
-						collaboraConfig.COLLABORA_ONLINE_URL = 'https://collabora.test';
-						httpService.get.mockReturnValueOnce(of(response));
+					collaboraConfig.COLLABORA_ONLINE_URL = 'https://collabora.test';
+					httpService.get.mockReturnValueOnce(of(response));
 
-						return {
-							mimeType,
-							urlsrc,
-						};
+					return {
+						mimeType,
+						urlsrc,
 					};
+				};
 
-					it('should return the discovery url for the given mimeType', async () => {
-						const { mimeType, urlsrc } = setup();
+				it('should return the discovery url for the given mimeType', async () => {
+					const { mimeType, urlsrc } = setup();
 
-						const result = await service.discoverUrl(mimeType);
+					const result = await service.discoverUrl(mimeType);
 
-						expect(result).toBe(urlsrc);
-					});
-				});
-
-				describe('when status is not 200', () => {
-					const setup = () => {
-						const mimeType = 'application/vnd.oasis.opendocument.text';
-						const response = axiosResponseFactory.build({ data: '', status: 404 });
-						const error = new Error('Request failed. Status Code: 404');
-
-						collaboraConfig.COLLABORA_ONLINE_URL = 'https://collabora.test';
-						httpService.get.mockReturnValueOnce(of(response));
-
-						return {
-							mimeType,
-							error,
-						};
-					};
-
-					it('should throw InternalServerErrorException when discovery fails', async () => {
-						const { mimeType, error } = setup();
-
-						await expect(service.discoverUrl(mimeType)).rejects.toThrow(
-							new InternalServerErrorException('DISCOVERY_SERVER_FAILED', { cause: error })
-						);
-					});
-				});
-
-				describe('when response data is not a valid XML', () => {
-					const setup = () => {
-						const mimeType = 'application/vnd.oasis.opendocument.text';
-						const response = axiosResponseFactory.build({ data: 'Invalid XML', status: 200 });
-						const error = new Error('missing root element');
-
-						collaboraConfig.COLLABORA_ONLINE_URL = 'https://collabora.test';
-						httpService.get.mockReturnValueOnce(of(response));
-
-						return {
-							mimeType,
-							error,
-						};
-					};
-
-					it('should throw InternalServerErrorException when discovery fails', async () => {
-						const { mimeType, error } = setup();
-
-						await expect(service.discoverUrl(mimeType)).rejects.toThrow(
-							new InternalServerErrorException('DISCOVERY_SERVER_FAILED', { cause: error })
-						);
-					});
-				});
-
-				describe('when response data is not containing action with mimetype', () => {
-					const setup = () => {
-						const mimeType = 'application/vnd.oasis.opendocument.text';
-						const xmlString = `<wopi-discovery><net-zone><app name='not-a-mimetype'><action urlsrc='https://collabora.test/hosting/discovery'/></app></net-zone></wopi-discovery>`;
-						const response = axiosResponseFactory.build({ data: xmlString, status: 200 });
-						const error = new Error('Type is not an array with elements.');
-
-						collaboraConfig.COLLABORA_ONLINE_URL = 'https://collabora.test';
-						httpService.get.mockReturnValueOnce(of(response));
-
-						return {
-							mimeType,
-							error,
-						};
-					};
-
-					it('should throw InternalServerErrorException when discovery fails', async () => {
-						const { mimeType, error } = setup();
-
-						await expect(service.discoverUrl(mimeType)).rejects.toThrow(
-							new InternalServerErrorException('DISCOVERY_SERVER_FAILED', { cause: error })
-						);
-					});
-				});
-
-				describe('when response data is not containing url', () => {
-					const setup = () => {
-						const mimeType = 'application/vnd.oasis.opendocument.text';
-						const xmlString = `<wopi-discovery><net-zone><app name='${mimeType}'><action/></app></net-zone></wopi-discovery>`;
-						const response = axiosResponseFactory.build({ data: xmlString, status: 200 });
-						const error = new Error('The requested mime type is not handled');
-
-						collaboraConfig.COLLABORA_ONLINE_URL = 'https://collabora.test';
-						httpService.get.mockReturnValueOnce(of(response));
-
-						return {
-							mimeType,
-							error,
-						};
-					};
-
-					it('should throw InternalServerErrorException when discovery fails', async () => {
-						const { mimeType, error } = setup();
-
-						await expect(service.discoverUrl(mimeType)).rejects.toThrow(
-							new InternalServerErrorException('DISCOVERY_SERVER_FAILED', { cause: error })
-						);
-					});
+					expect(result).toBe(urlsrc);
 				});
 			});
 
-			describe('when getDiscoveryUrl rejects', () => {
-				describe('when error is not axios error', () => {
-					const setup = () => {
-						const mimeType = 'application/vnd.oasis.opendocument.text';
-						const error = new Error('Request failed');
+			describe('when status is not 200', () => {
+				const setup = () => {
+					const mimeType = 'application/vnd.oasis.opendocument.text';
+					const response = axiosResponseFactory.build({ data: '', status: 404 });
+					const error = new Error('Request failed. Status Code: 404');
 
-						collaboraConfig.COLLABORA_ONLINE_URL = 'https://collabora.test';
-						httpService.get.mockReturnValueOnce(throwError(() => error));
+					collaboraConfig.COLLABORA_ONLINE_URL = 'https://collabora.test';
+					httpService.get.mockReturnValueOnce(of(response));
 
-						return {
-							mimeType,
-							error,
-						};
+					return {
+						mimeType,
+						error,
 					};
+				};
 
-					it('should throw InternalServerErrorException when discovery fails', async () => {
-						const { mimeType, error } = setup();
+				it('should throw InternalServerErrorException when discovery fails', async () => {
+					const { mimeType, error } = setup();
 
-						await expect(service.discoverUrl(mimeType)).rejects.toThrow(
-							new InternalServerErrorException('DISCOVERY_SERVER_FAILED', { cause: error })
-						);
-					});
+					await expect(service.discoverUrl(mimeType)).rejects.toThrow(
+						new InternalServerErrorException('DISCOVERY_SERVER_FAILED', { cause: error })
+					);
 				});
+			});
 
-				describe('when error is axios error', () => {
-					const setup = () => {
-						const mimeType = 'application/vnd.oasis.opendocument.text';
-						const error = new AxiosError();
+			describe('when response data is not a valid XML', () => {
+				const setup = () => {
+					const mimeType = 'application/vnd.oasis.opendocument.text';
+					const response = axiosResponseFactory.build({ data: 'Invalid XML', status: 200 });
+					const error = new Error('missing root element');
 
-						collaboraConfig.COLLABORA_ONLINE_URL = 'https://collabora.test';
-						httpService.get.mockReturnValueOnce(throwError(() => error));
+					collaboraConfig.COLLABORA_ONLINE_URL = 'https://collabora.test';
+					httpService.get.mockReturnValueOnce(of(response));
 
-						return {
-							mimeType,
-							error,
-						};
+					return {
+						mimeType,
+						error,
 					};
+				};
 
-					it('should throw InternalServerErrorException with AxiosErrorLoggable', async () => {
-						const { mimeType, error } = setup();
+				it('should throw InternalServerErrorException when discovery fails', async () => {
+					const { mimeType, error } = setup();
 
-						const axiosErrorLoggable = new AxiosErrorLoggable(error, 'DISCOVERY_SERVER_FAILED');
-						await expect(service.discoverUrl(mimeType)).rejects.toThrow(
-							new InternalServerErrorException('DISCOVERY_SERVER_FAILED', { cause: axiosErrorLoggable })
-						);
-					});
+					await expect(service.discoverUrl(mimeType)).rejects.toThrow(
+						new InternalServerErrorException('DISCOVERY_SERVER_FAILED', { cause: error })
+					);
+				});
+			});
+
+			describe('when response data is not containing action with mimetype', () => {
+				const setup = () => {
+					const mimeType = 'application/vnd.oasis.opendocument.text';
+					const xmlString = `<wopi-discovery><net-zone><app name='not-a-mimetype'><action urlsrc='https://collabora.test/hosting/discovery'/></app></net-zone></wopi-discovery>`;
+					const response = axiosResponseFactory.build({ data: xmlString, status: 200 });
+					const error = new Error('Type is not an array with elements.');
+
+					collaboraConfig.COLLABORA_ONLINE_URL = 'https://collabora.test';
+					httpService.get.mockReturnValueOnce(of(response));
+
+					return {
+						mimeType,
+						error,
+					};
+				};
+
+				it('should throw InternalServerErrorException when discovery fails', async () => {
+					const { mimeType, error } = setup();
+
+					await expect(service.discoverUrl(mimeType)).rejects.toThrow(
+						new InternalServerErrorException('DISCOVERY_SERVER_FAILED', { cause: error })
+					);
+				});
+			});
+
+			describe('when response data is not containing url', () => {
+				const setup = () => {
+					const mimeType = 'application/vnd.oasis.opendocument.text';
+					const xmlString = `<wopi-discovery><net-zone><app name='${mimeType}'><action/></app></net-zone></wopi-discovery>`;
+					const response = axiosResponseFactory.build({ data: xmlString, status: 200 });
+					const error = new Error('The requested mime type is not handled');
+
+					collaboraConfig.COLLABORA_ONLINE_URL = 'https://collabora.test';
+					httpService.get.mockReturnValueOnce(of(response));
+
+					return {
+						mimeType,
+						error,
+					};
+				};
+
+				it('should throw InternalServerErrorException when discovery fails', async () => {
+					const { mimeType, error } = setup();
+
+					await expect(service.discoverUrl(mimeType)).rejects.toThrow(
+						new InternalServerErrorException('DISCOVERY_SERVER_FAILED', { cause: error })
+					);
+				});
+			});
+		});
+
+		describe('when getDiscoveryUrl rejects', () => {
+			describe('when error is not axios error', () => {
+				const setup = () => {
+					const mimeType = 'application/vnd.oasis.opendocument.text';
+					const error = new Error('Request failed');
+
+					collaboraConfig.COLLABORA_ONLINE_URL = 'https://collabora.test';
+					httpService.get.mockReturnValueOnce(throwError(() => error));
+
+					return {
+						mimeType,
+						error,
+					};
+				};
+
+				it('should throw InternalServerErrorException when discovery fails', async () => {
+					const { mimeType, error } = setup();
+
+					await expect(service.discoverUrl(mimeType)).rejects.toThrow(
+						new InternalServerErrorException('DISCOVERY_SERVER_FAILED', { cause: error })
+					);
+				});
+			});
+
+			describe('when error is axios error', () => {
+				const setup = () => {
+					const mimeType = 'application/vnd.oasis.opendocument.text';
+					const error = new AxiosError();
+
+					collaboraConfig.COLLABORA_ONLINE_URL = 'https://collabora.test';
+					httpService.get.mockReturnValueOnce(throwError(() => error));
+
+					return {
+						mimeType,
+						error,
+					};
+				};
+
+				it('should throw InternalServerErrorException with AxiosErrorLoggable', async () => {
+					const { mimeType, error } = setup();
+
+					const axiosErrorLoggable = new AxiosErrorLoggable(error, 'DISCOVERY_SERVER_FAILED');
+					await expect(service.discoverUrl(mimeType)).rejects.toThrow(
+						new InternalServerErrorException('DISCOVERY_SERVER_FAILED', { cause: axiosErrorLoggable })
+					);
 				});
 			});
 		});
