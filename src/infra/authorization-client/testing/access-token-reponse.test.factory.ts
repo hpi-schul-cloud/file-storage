68290d47ef9b55adfe435ed13d54b33c68e4c214
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2025-08-27 14:01:04 +0200

    Remove math operation to solve sonar cloud notification.

--- src/infra/authorization-client/testing/access-token-reponse.test.factory.ts
+++ src/infra/authorization-client/testing/access-token-reponse.test.factory.ts
@@ -1,11 +1,13 @@
+import { pseudoRandomBytes } from 'node:crypto';
 import type { AccessTokenResponse } from '../authorization-api-client/models/access-token-response';
 
 const generateNanoId = (length = 24): string => {
 	const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-';
+	const bytes = pseudoRandomBytes(length);
 	let result = '';
+
 	for (let i = 0; i < length; i++) {
-		const randomIndex = Math.floor(Math.random() * chars.length);
-		result += chars[randomIndex];
+		result += chars[bytes[i] % chars.length];
 	}
 
 	return result;
--- src/modules/files-storage/testing/wopi/wopi-access-token-params.test.factory.ts
+++ src/modules/files-storage/testing/wopi/wopi-access-token-params.test.factory.ts
@@ -1,11 +1,13 @@
+import { pseudoRandomBytes } from 'node:crypto';
 import { WopiAccessTokenParams } from '../../api/dto';
 
 const generateNanoId = (length = 24): string => {
 	const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-';
+	const bytes = pseudoRandomBytes(length);
 	let result = '';
+
 	for (let i = 0; i < length; i++) {
-		const randomIndex = Math.floor(Math.random() * chars.length);
-		result += chars[randomIndex];
+		result += chars[bytes[i] % chars.length];
 	}
 
 	return result;
