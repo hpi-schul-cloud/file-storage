0a627a8a1bd40e1f888526d5ec5cad65c764e4a5
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2025-08-27 13:44:57 +0200

    Replace code with existing solution to replace uuid with nanoid like string.

--- src/infra/authorization-client/testing/access-token-reponse.test.factory.ts
+++ src/infra/authorization-client/testing/access-token-reponse.test.factory.ts
@@ -1,9 +1,19 @@
-import { randomUUID } from 'crypto';
 import type { AccessTokenResponse } from '../authorization-api-client/models/access-token-response';
+
+const generateNanoId = (length = 24): string => {
+	const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-';
+	let result = '';
+	for (let i = 0; i < length; i++) {
+		const randomIndex = Math.floor(Math.random() * chars.length);
+		result += chars[randomIndex];
+	}
+
+	return result;
+};
 
 class AccessTokenResponseTestFactory {
 	private readonly props: AccessTokenResponse = {
-		token: randomUUID(),
+		token: generateNanoId(),
 	};
 
 	public build(params: Partial<AccessTokenResponse> = {}): AccessTokenResponse {
