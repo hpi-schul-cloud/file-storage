022b0eb676bdf81cef03ddba805bf0f67009ead4
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2025-08-27 13:08:11 +0200

    Add Method to generate nanoId like token.

--- src/modules/files-storage/testing/wopi/wopi-access-token-params.test.factory.ts
+++ src/modules/files-storage/testing/wopi/wopi-access-token-params.test.factory.ts
@@ -1,9 +1,19 @@
-import { randomUUID } from 'node:crypto';
 import { WopiAccessTokenParams } from '../../api/dto';
+
+const generateNanoId = (length = 24): string => {
+	const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-';
+	let result = '';
+	for (let i = 0; i < length; i++) {
+		const randomIndex = Math.floor(Math.random() * chars.length);
+		result += chars[randomIndex];
+	}
+
+	return result;
+};
 
 class WopiAccessTokenParamsTestFactory {
 	private readonly props: WopiAccessTokenParams = {
-		access_token: randomUUID(),
+		access_token: generateNanoId(),
 	};
 
 	public build(params: Partial<WopiAccessTokenParams> = {}): WopiAccessTokenParams {
