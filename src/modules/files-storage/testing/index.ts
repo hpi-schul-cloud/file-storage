f219a0e2b122dbc57a7ae7498f3ec4dba95f9017
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2025-09-22 13:05:35 +0200

    revert wrong move of testing factory

--- src/modules/files-storage/testing/index.ts
+++ src/modules/files-storage/testing/index.ts
@@ -1,3 +1,4 @@
+export * from './busboy-file-info.test.factory';
 export * from './file-dto.test.factory';
 export * from './file-record-params.test.factory';
 export * from './file-record-security-check.embeddable.factory';
--- src/modules/wopi/testing/index.ts
+++ src/modules/wopi/testing/index.ts
@@ -1,6 +1,5 @@
 export * from './access-url.test.factory';
 export * from './authorized-collabora-document-url.params.test.factory';
-export * from './busboy-file-info.test.factory';
 export * from './wopi-access-token-params.test.factory';
 export * from './wopi-access-token.test.factory';
 export * from './wopi-payload.test.factory';
