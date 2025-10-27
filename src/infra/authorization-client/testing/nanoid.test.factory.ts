1f1314d517f3bb8b920a541780f4ce5e25d52fa5
Max Bischof <maximilian.bischof@dataport.de>
2025-10-27 15:43:51 +0100

    Set ts config noUncheckedIndexedAccess to true

--- src/infra/authorization-client/testing/nanoid.test.factory.ts
+++ src/infra/authorization-client/testing/nanoid.test.factory.ts
@@ -3,11 +3,6 @@ import { pseudoRandomBytes } from 'node:crypto';
 export const generateNanoId = (length = 24): string => {
 	const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-';
 	const bytes = pseudoRandomBytes(length);
-	let result = '';
 
-	for (let i = 0; i < length; i++) {
-		result += chars[bytes[i] % chars.length];
-	}
-
-	return result;
+	return Array.from(bytes, (byte) => chars[byte % chars.length]).join('');
 };
