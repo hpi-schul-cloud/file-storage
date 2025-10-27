21a87c85b1b5a67ab7969a65f4b56c2d754be19d
Phillip Wirth <phillip.wirth@dataport.de>
2025-10-27 14:29:27 +0100

    because i can

--- src/infra/metrics/interceptor/current-upload-metrics.interceptor.ts
+++ src/infra/metrics/interceptor/current-upload-metrics.interceptor.ts
@@ -7,13 +7,11 @@ import { MetricsService } from '../metrics.service';
 export class CurrentUploadMetricsInterceptor implements NestInterceptor {
 	public async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<unknown>> {
 		MetricsService.currentUploadsGauge.inc();
-		console.log("in " + new Date() + JSON.stringify(await MetricsService.currentUploadsGauge.get()))
 
 		const uploadDurationTimer = MetricsService.uploadDurationHistogram.startTimer();
 
 		return next.handle().pipe(
 			finalize(async () => {
-				console.log("out " + new Date() + JSON.stringify(await MetricsService.currentUploadsGauge.get()))
 				MetricsService.currentUploadsGauge.dec();
 				uploadDurationTimer();
 			})
