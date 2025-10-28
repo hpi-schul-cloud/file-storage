e61390cfd30c349b7e30c7e1522c0f1ff869f5ad
Max Bischof <maximilian.bischof@dataport.de>
2025-10-28 15:18:33 +0100

    refactor: store generated ID in a variable before returning

--- src/infra/authorization-client/testing/nanoid.test.factory.ts
+++ src/infra/authorization-client/testing/nanoid.test.factory.ts
@@ -4,5 +4,7 @@ export const generateNanoId = (length = 24): string => {
 	const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-';
 	const bytes = pseudoRandomBytes(length);
 
-	return Array.from(bytes, (byte) => chars[byte % chars.length]).join('');
+	const id = Array.from(bytes, (byte) => chars[byte % chars.length]).join('');
+
+	return id;
 };
