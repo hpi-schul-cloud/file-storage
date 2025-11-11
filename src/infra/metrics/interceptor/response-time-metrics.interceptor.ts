3c28c5b9870418f5892278287b5efada52b98481
SevenWaysDP <sergej.hoffmann@dataport.de>
2025-11-11 09:21:52 +0100

    Refactor ResponseTimeMetricsInterceptor to calculate and observe response duration in seconds

--- src/infra/metrics/interceptor/response-time-metrics.interceptor.ts
+++ src/infra/metrics/interceptor/response-time-metrics.interceptor.ts
@@ -1,20 +1,25 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { Request, Response } from 'express';
-import { Observable } from 'rxjs';
+import { finalize, Observable } from 'rxjs';
 import { RequestResponseMetricLabelFactory } from '../factory';
 import { MetricsService } from '../metrics.service';
 
 @Injectable()
 export class ResponseTimeMetricsInterceptor implements NestInterceptor {
 	public intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
-		const time = Date.now();
+		const startTime = Date.now();
 		const request = context.switchToHttp().getRequest<Request>();
 		const response = context.switchToHttp().getResponse<Response>();
 		const label = RequestResponseMetricLabelFactory.create(request, response);
-		const timeInSeconds = time / 1000;
 
-		MetricsService.responseTimeMetricHistogram.observe(label, timeInSeconds);
+		return next.handle().pipe(
+			finalize(() => {
+				const endTime = Date.now();
+				const durationMs = endTime - startTime;
+				const durationSeconds = durationMs / 1000;
 
-		return next.handle();
+				MetricsService.responseTimeMetricHistogram.observe(label, durationSeconds);
+			})
+		);
 	}
 }
