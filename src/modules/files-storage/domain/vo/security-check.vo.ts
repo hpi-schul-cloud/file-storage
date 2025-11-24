b815b5f70fdc3ed6bd002870268889551ca6b33b
Constantin Bergatt <constantin.bergatt@dataport.de>
2025-11-24 10:45:03 +0100

    BC-10968 - Remove optional validation from reason in FileRecordSecurityCheck

--- src/modules/files-storage/domain/vo/security-check.vo.ts
+++ src/modules/files-storage/domain/vo/security-check.vo.ts
@@ -23,7 +23,6 @@ export class FileRecordSecurityCheck implements FileRecordSecurityCheckProps {
 	public readonly status: ScanStatus;
 
 	@IsString()
-	@IsOptional()
 	public readonly reason: string;
 
 	@IsUUID()
