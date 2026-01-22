69dd5aa1acc3ba29e011d60a3d6da962c8fb4f2b
SevenWaysDP <sergej.hoffmann@dataport.de>
2026-01-22 10:25:57 +0100

    BC-11180 - Refactor detectMimeTypeByStream to improve memory management and clarity

--- src/modules/files-storage/domain/utils/detect-mime-type.utils.ts
+++ src/modules/files-storage/domain/utils/detect-mime-type.utils.ts
@@ -40,12 +40,18 @@ export async function detectMimeTypeByStream(passThrough: PassThrough, fallbackM
 	if (!isFileTypePackageSupported(fallbackMimeType)) {
 		return fallbackMimeType;
 	}
-
 	/* istanbul ignore next */
 	const fileTypeStreamResult = await fileTypeStream(passThrough);
 
 	/* istanbul ignore next */
-	return resolveMimeType(fileTypeStreamResult, fallbackMimeType);
+	const mimeType = resolveMimeType(fileTypeStreamResult, fallbackMimeType);
+
+	// Clean up the fileTypeStream to prevent memory leaks
+	if (fileTypeStreamResult && typeof fileTypeStreamResult.destroy === 'function') {
+		fileTypeStreamResult.destroy();
+	}
+
+	return mimeType;
 }
 
 export default { detectMimeTypeByStream, fileTypeStream };
