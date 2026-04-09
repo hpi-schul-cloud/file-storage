d298a5fc4c3471933da8c273e31182ad9192a825
Cedric Evers <12080057+CeEv@users.noreply.github.com>
2026-04-09 16:56:22 +0200

    Revert "Add missing test if rules already are up to date."
    This reverts commit 7b1859402840c1f4ebaf4dbf1d51a99f4bdd5756.

--- src/modules/files-storage/repo/mapper/file-record.entity.mapper.ts
+++ src/modules/files-storage/repo/mapper/file-record.entity.mapper.ts
@@ -1,5 +1,5 @@
 import { EntityManager } from '@mikro-orm/mongodb';
-import { FileRecord, FileRecordFactory, FileRecordSecurityCheck } from '../../domain';
+import { FileRecord, FileRecordFactory, FileRecordSecurityCheck, StorageType } from '../../domain';
 import { FileRecordEntity } from '../file-record.entity';
 import { FileRecordSecurityCheckEmbeddable } from '../security-check.embeddable';
 
@@ -10,11 +10,10 @@ export class FileRecordEntityMapper {
 			return fileRecordEntity.domainObject;
 		}
 
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		const { securityCheck: securityCheckEmbeddable, domainObject, ...fileRecordProps } = fileRecordEntity;
+		const { securityCheck: securityCheckEmbeddable, ...fileRecordProps } = fileRecordEntity;
 		// we need to "copy" the "id" property manually, as otherwise the "id" will get lost
 		fileRecordProps.id = fileRecordEntity.id;
-		// fileRecordProps.storageType ??= StorageType.STANDARD;
+		fileRecordProps.storageType ??= StorageType.STANDARD;
 		const securityCheck = new FileRecordSecurityCheck(securityCheckEmbeddable);
 		// It is very important to hand over "fileRecordProps" CLEAN (meaning no additional properties)!!!
 		const fileRecord = FileRecordFactory.buildFromFileRecordProps(fileRecordProps, securityCheck);
@@ -27,11 +26,12 @@ export class FileRecordEntityMapper {
 
 	public static mapDoToEntity(em: EntityManager, fileRecord: FileRecord): FileRecordEntity {
 		const props = fileRecord.getProps();
-		const { ...restProps } = props;
+		const { storageType, ...restProps } = props;
+		const entityStorageType = storageType === StorageType.STANDARD ? undefined : storageType;
 
 		const entity =
 			em.getUnitOfWork().getById<FileRecordEntity>(FileRecordEntity.name, props.id) ?? new FileRecordEntity();
-		em.assign(entity, { ...restProps });
+		em.assign(entity, { ...restProps, storageType: entityStorageType });
 
 		entity.securityCheck = FileRecordEntityMapper.mapDoToEmbeddable(em, fileRecord);
 
