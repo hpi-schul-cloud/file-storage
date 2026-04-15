b557bc52578e4b4224623c630163f7e9d580fde7
Sergej Hoffmann <97111299+SevenWaysDP@users.noreply.github.com>
2026-04-15 14:45:30 +0200

    BC-11601 - FileStorage is unstable after updating the file-type package (#215)

--- src/modules/files-storage/domain/utils/file-type-stream.import.ts
+++ src/modules/files-storage/domain/utils/file-type-stream.import.ts
@@ -1,12 +1,13 @@
 import type { FileTypeResult } from 'file-type';
 import { loadEsm } from 'load-esm';
-import { Readable } from 'stream';
+import { Readable } from 'node:stream';
+import { DEFAULT_CHUNK_SIZE } from './stream.utils';
 
 /* istanbul ignore next */
 export async function detectFileTypeFromStream(file: Readable): Promise<FileTypeResult | undefined> {
 	const { fileTypeFromStream } = await loadEsm<typeof import('file-type')>('file-type');
 
-	const webStream = Readable.toWeb(file);
+	const webStream = Readable.toWeb(file, { strategy: { highWaterMark: DEFAULT_CHUNK_SIZE } });
 	const fileType = await fileTypeFromStream(webStream);
 
 	return fileType;
