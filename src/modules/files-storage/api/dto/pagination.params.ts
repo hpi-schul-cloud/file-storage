aba2e3600afb6b1a3f1ecb1f53d907a0d4047415
Max Bischof <maximilian.bischof@dataport.de>
2026-04-30 07:35:44 +0200

    Remove default values for pagination parameters in PaginationParams class

--- src/modules/files-storage/api/dto/pagination.params.ts
+++ src/modules/files-storage/api/dto/pagination.params.ts
@@ -1,15 +1,15 @@
-import { IsInt, Max, Min } from 'class-validator';
 import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsInt, Max, Min } from 'class-validator';
 
 export class PaginationParams {
 	@IsInt()
 	@Min(0)
 	@ApiPropertyOptional({ description: 'Number of elements (not pages) to be skipped' })
-	skip?: number = 0;
+	skip?: number;
 
 	@IsInt()
 	@Min(1)
 	@Max(100)
 	@ApiPropertyOptional({ description: 'Page limit, defaults to 10.', minimum: 1, maximum: 99 })
-	limit?: number = 10;
+	limit?: number;
 }
