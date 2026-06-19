6f3f0764bf722cc3d8213435b35a61830ca8ebea
Max Bischof <maximilian.bischof@dataport.de>
2026-06-19 11:37:53 +0200

    Refactor AuthorizationClientModule to improve HTTP agent handling and support both HTTP and HTTPS configurations

--- src/infra/authorization-client/authorization-client.module.ts
+++ src/infra/authorization-client/authorization-client.module.ts
@@ -1,7 +1,8 @@
 import { ConfigurationModule } from '@infra/configuration';
 import { DynamicModule, Module } from '@nestjs/common';
 import axios from 'axios';
-import { Agent } from 'node:https';
+import { Agent as HttpAgent } from 'node:http';
+import { Agent as HttpsAgent } from 'node:https';
 import { AuthorizationApi, Configuration, ConfigurationParameters } from './authorization-api-client';
 import { AuthorizationClientAdapter } from './authorization-client.adapter';
 import { AUTHORIZATION_CONFIG_TOKEN, AuthorizationConfig } from './authorization.config';
@@ -20,8 +21,11 @@ export class AuthorizationClientModule {
 				useFactory: (config: AuthorizationConfig): AuthorizationApi => {
 					const configuration = new Configuration({ basePath: config.authorizationApiUrl });
 
-					const httpsAgent = new Agent({ keepAlive: true, maxSockets: config.maxSockets, keepAliveMsecs: 10_000 });
-					const axiosInstance = axios.create({ httpsAgent });
+					const agentOptions = { keepAlive: true, maxSockets: config.maxSockets, keepAliveMsecs: 10_000 };
+					const isHttps = config.authorizationApiUrl.startsWith('https://');
+					const axiosInstance = isHttps
+						? axios.create({ httpsAgent: new HttpsAgent(agentOptions) })
+						: axios.create({ httpAgent: new HttpAgent(agentOptions) });
 
 					return new AuthorizationApi(configuration, config.authorizationApiUrl, axiosInstance);
 				},
